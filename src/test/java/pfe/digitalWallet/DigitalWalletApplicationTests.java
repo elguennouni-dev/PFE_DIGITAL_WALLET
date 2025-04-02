482b830400dbfb90791819c6d423ced4b16ae7c8
Abdelilah El-Guennouni <abdlilah.el.guennouni@gmail.com>
2025-04-02 18:12:45 +0000

    Resolved merge conflicts

---
+++ src/test/java/pfe/digitalWallet/DigitalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.digitalWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
