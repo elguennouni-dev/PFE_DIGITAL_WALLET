4bef8a58fd1c14a3cc7d1490e4c679c1dcdbd4da
Abdelilah El-Guennouni <abdlilah.el.guennouni@gmail.com>
2025-04-04 13:37:02 +0000

    Last Updates...

--- src/main/java/pfe/digitalWallet/configuration/SecurityConfig.java
+++ src/main/java/pfe/digitalWallet/configuration/SecurityConfig.java
@@ -1,12 +1,31 @@
 package pfe.digitalWallet.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-//    public static class CorsFilter extends OncePerRequestFilter {
-//
-//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
