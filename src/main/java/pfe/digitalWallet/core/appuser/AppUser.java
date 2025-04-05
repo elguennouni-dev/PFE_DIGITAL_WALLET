c98ccac72a52b3823b65554c7b0a54cd72315fec
Yassin <abdelilah.freelance@gmail.com>
2025-04-05 16:25:48 +0000

    ii

--- src/main/java/pfe/digitalWallet/core/appuser/AppUser.java
+++ src/main/java/pfe/digitalWallet/core/appuser/AppUser.java
@@ -3,12 +3,11 @@ package pfe.digitalWallet.core.appuser;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
-import lombok.*;
+import lombok.Data;
 import pfe.digitalWallet.core.document.Document;
 import pfe.digitalWallet.core.loginattempt.LoginAttempt;
 import pfe.digitalWallet.core.loginhistory.LoginHistory;
 import pfe.digitalWallet.core.session.Session;
-import pfe.digitalWallet.shared.validation.annotation.ValidLocalDateTime;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -17,9 +16,6 @@ import java.util.List;
 @Table(name = "app_user")
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter @Setter
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,24 +34,18 @@ public class AppUser {
     @Size(min = 8, max = 32, message = "Password must be between 8 and 32 characters")
     private String password;
 
-    @NotNull(message = "Password cannot be Null")
-    @NotEmpty(message = "Password cannot be Empty")
-    @NotBlank(message = "Password cannot be Blank")
+    @NotNull(message = "Email cannot be Null")
+    @NotEmpty(message = "Email cannot be Empty")
+    @NotBlank(message = "Email cannot be Blank")
     @Email(message = "Email should be valid")
     private String email;
 
-//    @NotNull(message = "Creation date-time cannot be Null")
-//    @NotEmpty(message = "Creation date-time cannot be Empty")
-//    @NotBlank(message = "Creation date-time cannot be Blank")
-//    @PastOrPresent(message = "Creation date-time cannot be in the future")
-    @ValidLocalDateTime(message = "Creation date-time cannot be null or in the future")
+    @NotNull(message = "Creation date-time cannot be Null")
+    @PastOrPresent(message = "Creation date-time cannot be in the future")
     private LocalDateTime createdAt;
 
-//    @NotNull(message = "Update date-time cannot be Null")
-//    @NotEmpty(message = "Update date-time cannot be Empty")
-//    @NotBlank(message = "Update date-time cannot be Blank")
-//    @PastOrPresent(message = "Update date-time cannot be in the future")
-    @ValidLocalDateTime(message = "Update date-time cannot be null or in the future")
+    @NotNull(message = "Update date-time cannot be Null")
+    @PastOrPresent(message = "Update date-time cannot be in the future")
     private LocalDateTime updatedAt;
 
     @Valid
