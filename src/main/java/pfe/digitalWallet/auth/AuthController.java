53d15b38c24d573557a24477f2bbac5fab1aa733
Abdelilah El-Guennouni <abdlilah.el.guennouni@gmail.com>
2025-04-05 13:25:12 +0000

    Authentication updates...

--- src/main/java/pfe/digitalWallet/auth/AuthController.java
+++ src/main/java/pfe/digitalWallet/auth/AuthController.java
@@ -1,10 +1,13 @@
 package pfe.digitalWallet.auth;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pfe.digitalWallet.shared.dto.ApiResponse;
 import pfe.digitalWallet.shared.dto.LoginRequest;
 import pfe.digitalWallet.shared.dto.UserDto;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,8 +21,27 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<UserDto>> login(@RequestBody LoginRequest loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
 
+        Optional<UserDto> userDtoOptional = authService.login(username, password);
+
+        if(userDtoOptional.isEmpty()) {
+            return buildResponse(false, "Invalid username or password", null, HttpStatus.UNAUTHORIZED);
+        }
+        return buildResponse(true, "Login successful", userDtoOptional.get(), HttpStatus.OK);
     }
 
+
+
+
+    // Private helper methods
+    private ResponseEntity<ApiResponse<UserDto>> buildResponse(boolean success, String message, UserDto data, HttpStatus status) {
+        ApiResponse<UserDto> response = new ApiResponse<>();
+        response.setSuccess(success);
+        response.setMessage(message);
+        response.setData(data);
+        return ResponseEntity.status(status).body(response);
+    }
 
 }
